7ea8981ea50f5f70ffa98e759e7603e7422f22e4
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-08-12 21:57:35 -0400

    Added UTs for camel case function

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
+import App, { replaceCamelWithSpaces } from './App';
 
 test('button turns blue when clicked', () => {
 	// arrange
@@ -62,4 +62,16 @@ test('initial condition', () => {
 	// check that the checkbox starts out unchecked
 	const checkbox = screen.getByRole('checkbox');
 	expect(checkbox).not.toBeChecked();
+});
+
+describe('spaces before camel-case capital letters', () => {
+	test('Works for no inner capital letters', () => {
+		expect(replaceCamelWithSpaces('Red')).toBe('Red');
+	});
+	test('Works for one inner capital letter', () => {
+		expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+	});
+	test('Works for multiple inner capital letters', () => {
+		expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+	});
 });
